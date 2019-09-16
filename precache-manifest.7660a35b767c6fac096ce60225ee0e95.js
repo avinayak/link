e85ee3506aec264db4381f877bcce876879a2d5c
Atul Vinayak <su.atul.vi@gmail.com>
2019-09-16 23:06:33 +0900

    Updates

---
+++ precache-manifest.7660a35b767c6fac096ce60225ee0e95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6ec7e84ef7c8d1f9621450c9a6e92e4",
+    "url": "/link/index.html"
+  },
+  {
+    "revision": "383ef97a4882b363bc89",
+    "url": "/link/static/css/main.a13a7095.chunk.css"
+  },
+  {
+    "revision": "3d069a80de8290a3c726",
+    "url": "/link/static/js/2.1a3a6565.chunk.js"
+  },
+  {
+    "revision": "383ef97a4882b363bc89",
+    "url": "/link/static/js/main.39460752.chunk.js"
+  },
+  {
+    "revision": "46fa393e9702daf0f9b7",
+    "url": "/link/static/js/runtime~main.b93ce9e8.js"
+  }
+]);
